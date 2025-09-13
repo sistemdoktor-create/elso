d748afffc76de9fc1d48fd231d2156a61bec913c
herman <sistemdoktor@gmail.com>
2025-09-13 15:10:36 +0200

    monogram hozzáadása

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Monogram: HR.
+# Monogram: HR
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
